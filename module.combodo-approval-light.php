03525700304b65c070480824e2dd3af9321fcc18
Pierre Goiffon <pierre.goiffon@combodo.com>
2018-06-26 12:42:32 +0000

    combodo-approval-light : wooops fix version number
    SVN:1.1.3[6786]

--- module.combodo-approval-light.php
+++ module.combodo-approval-light.php
@@ -16,7 +16,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'combodo-approval-light/1.1.2',
+	'combodo-approval-light/1.1.3',
 	array(
 		// Identification
 		//
@@ -40,7 +40,7 @@ SetupWebPage::AddModule(
 			'main.combodo-approval-light.php'
 		),
 		'webservice' => array(
-			
+
 		),
 		'data.struct' => array(
 			// add your 'structure' definition XML files here,
@@ -48,11 +48,11 @@ SetupWebPage::AddModule(
 		'data.sample' => array(
 			// add your sample data XML files here,
 		),
-		
+
 		// Documentation
 		//
 		'doc.manual_setup' => '', // hyperlink to manual setup documentation, if any
-		'doc.more_information' => '', // hyperlink to more information, if any 
+		'doc.more_information' => '', // hyperlink to more information, if any
 
 		// Default settings
 		//
