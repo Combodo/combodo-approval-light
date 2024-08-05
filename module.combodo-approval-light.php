f88b4cff1b49bb57edc418c9cf0af2aac19b6753
denis.flaven@combodo.com <denis.flaven@combodo.com>
2024-08-05 16:36:44 +0200

    Version 1.2.4

--- module.combodo-approval-light.php
+++ module.combodo-approval-light.php
@@ -16,7 +16,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'combodo-approval-light/1.2.3',
+	'combodo-approval-light/1.2.4',
 	array(
 		// Identification
 		//
